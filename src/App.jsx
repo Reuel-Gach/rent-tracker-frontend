ecc20e8d896b4db27f08573184e65ff9cc0b2ad5
Reuel-Gach <reuel@laikipia.ac.ke>
2026-05-26 17:34:06 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LayoutDashboard, Calculator, FileEdit } from 'lucide-react';
 import WaterCalculator from './pages/WaterCalculator';
 import DataEntry from './pages/DataEntry';
 import Dashboard from './pages/Dashboard';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
